c829c91433aebca4e50097a8da614cc2f9dfdf00
n1kpavlov <nikniknik388@gmail.com>
2024-06-05 20:15:47 +0300

    Update Class_7.cpp

--- exercises/4_1_3/Class_7.cpp
+++ exercises/4_1_3/Class_7.cpp
@@ -1,7 +1,6 @@
 #include "Class_7.h"
 
-Class_7::Class_7(string s): Class_1(s + "_7"), Class_4(s + "_7"), Class_5(s +
-	"_7")
+Class_7::Class_7(string s): Class_1(s + "_7"), Class_4(s + "_7"), Class_5(s + "_7")
 {
 	name = s + "_7";
 }

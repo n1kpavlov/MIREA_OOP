11c48b91b0a0462614390e7f30b09ea041c861fa
Pavlov Nikita <nikniknik388@gmail.com>
2024-12-18 17:03:32 +0300

    Create main.cpp

---
+++ coursework/kv-1/main.cpp
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Cl_application.h"
+int main()
+{
+	Cl_application ob_cl_application(nullptr);//создание объекта приложения
+	ob_cl_application.build_tree_objects();//конструирование системы
+	return ob_cl_application.exec_app();//запуск системы
+}
